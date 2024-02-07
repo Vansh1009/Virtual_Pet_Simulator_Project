d2e909d1443bf757d6c430cd4104f436269e47ea
vansh <vpalkhiwala1360@conestogac.on.ca>
2024-02-06 22:32:54 -0500

    Add project files.

---
+++ Virtual_Pet_Simulator_Project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VirtualPetSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Virtual Pet Simulator!");
+            Console.WriteLine("Choose a pet type: 1. Cat, 2. Dog, 3. Rabbit");
+            int petType = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your pet's name:");
+            string petName = Console.ReadLine();
+
+        }
+    }
+}
